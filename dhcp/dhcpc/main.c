cad2c57bb53beaa6783b8c8c208220a503363720
ainozaki <aiai10piano@gmail.com>
2022-07-11 16:13:40 +0900

    dhcp: send/recv udp

--- dhcp/dhcpc/main.c
+++ dhcp/dhcpc/main.c
@@ -1,6 +1,71 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
-int main(){
-	printf("Hello world\n");
+/*
+// Internet address 
+struct in_addr {
+	in_addr_t s_addr;
+};
+
+// Socket address, internet style.
+struct sockaddr_in {
+	uint8_t sin_len;
+	sa_family_t sin_family; // アドレスファミリー (AF_INET)
+	in_port_t sin_port; // ポート番号
+	struct in_addr sin_addr; // IP アドレス
+	char sin_zero[8];
+};
+*/
+
+void send_to_server(const char *server_ip){
+	int s, count, datalen;
+	struct sockaddr_in skt;
+	in_port_t port;
+	struct in_addr ipaddr;
+	char sbuf[512];
+
+	printf("sending to %s...\n", server_ip);
+
+	// socket
+	if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+		fprintf(stderr, "cannot open socket");
+		return;
+	}
+
+	// set sockaddr_in
+	memset(&skt, 0, sizeof(skt));
+	skt.sin_family = AF_INET;
+	port = 51230;
+	skt.sin_port = htons(port);
+	if (inet_aton(server_ip, &ipaddr) != 1){
+		fprintf(stderr, "cannot convert %s\n", server_ip);
+		return;
+	}
+	skt.sin_addr.s_addr = ipaddr.s_addr;
+	
+	// send
+	memset(sbuf, 'a', 512);
+	datalen = 128;
+	if ((count = sendto(s, sbuf, datalen, 0, (struct sockaddr *)&skt, sizeof(skt))) < 0){
+		perror("sendto");
+		return;
+	}
+	printf("send %d byte\n", count);
+}
+
+int main(int argc, char *argv[]){
+	if (argc < 2){
+		fprintf(stderr, "usage: %s <server-IP-address>\n", argv[0]);
+		return 1;
+	}
+	printf("starting dhcpc\n");
+
+	// connect to server
+	send_to_server(argv[1]);
+
 	return 0;
 }
---
+++ dhcp/dhcpd/main.c
@@ -0,0 +1,70 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+/*
+// Internet address
+struct in_addr {
+        in_addr_t s_addr;
+};
+
+// Socket address, internet style.
+struct sockaddr_in {
+        uint8_t sin_len;
+        sa_family_t sin_family; // アドレスファミリー (AF_INET)
+        in_port_t sin_port; // ポート番号
+        struct in_addr sin_addr; // IP アドレス
+        char sin_zero[8];
+};
+*/
+
+void wait_client() {
+  int s, count;
+  struct sockaddr_in myskt;
+  struct sockaddr_in skt;
+  socklen_t sktlen;
+  in_port_t port;
+  char rbuf[512];
+
+  printf("waiting for client...\n");
+
+  // socket
+  if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    perror("socket");
+    return;
+  }
+
+  // bind
+  memset(&myskt, 0, sizeof(myskt));
+  myskt.sin_family = AF_INET;
+  port = 51230;
+  myskt.sin_port = htons(port);
+  myskt.sin_addr.s_addr = htonl(INADDR_ANY);
+  if (bind(s, (struct sockaddr *)&myskt, sizeof(myskt)) < 0) {
+    perror("bind");
+    return;
+  }
+
+  // recv
+  sktlen = sizeof(skt);
+  if ((count = recvfrom(s, rbuf, sizeof(rbuf), 0, (struct sockaddr *)&skt,
+                        &sktlen)) < 0) {
+    perror("recvfrom");
+    return;
+  }
+  printf("received %d byte\n", count);
+
+  // send
+}
+
+int main(int argc, char *argv[]) {
+  printf("starting dhcpd\n");
+
+  // connect to server
+  wait_client();
+
+  return 0;
+}
