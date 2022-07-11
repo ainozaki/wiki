e618d7d52875a76cf7eb73e69cfc4614c299fe81
ainozaki <aiai10piano@gmail.com>
2022-07-11 18:23:50 +0900

    dhcp: send offer

--- dhcp/dhcpd/include/dhcpd.h
+++ dhcp/dhcpd/include/dhcpd.h
@@ -1,8 +1,22 @@
 #pragma once
 
+#define DEFAULT_TTL 64
+
 enum server_state {
   INIT,
   WAIT_DISCOVER,
   WAIT_REQUEST,
   ALLOCATED,
+};
+
+struct client {
+	struct client *fp;
+	struct client *bp;
+	short status;
+	int ttlcounter;
+	// network order
+	struct in_addr id;
+	struct in_addr addr;
+	struct in_addr netmask;
+	uint16_t ttl;
 };
--- dhcp/dhcpd/main.c
+++ dhcp/dhcpd/main.c
@@ -1,6 +1,7 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -24,6 +25,8 @@ struct sockaddr_in {
 };
 */
 int status;
+// TODO: create client list
+struct client client;
 
 const char *status_tbl[] = {
     "INIT",
@@ -37,20 +40,24 @@ void change_status(int nextst) {
   status = nextst;
 }
 
-void wait_client() {
+void init(){
+	printf("Inialize server\n");
+}
+
+struct mydhcph *wait_client() {
   int s, count;
   struct sockaddr_in myskt;
   struct sockaddr_in skt;
   socklen_t sktlen;
   in_port_t port;
-  char rbuf[512];
+  char *rbuf = malloc(sizeof(char) * 512);
 
   printf("waiting for client...\n");
 
   // socket
   if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
     perror("socket");
-    return;
+    return NULL;
   }
 
   // bind
@@ -61,7 +68,7 @@ void wait_client() {
   myskt.sin_addr.s_addr = htonl(INADDR_ANY);
   if (bind(s, (struct sockaddr *)&myskt, sizeof(myskt)) < 0) {
     perror("bind");
-    return;
+    return NULL;
   }
 
   // recv
@@ -69,11 +76,68 @@ void wait_client() {
   if ((count = recvfrom(s, rbuf, sizeof(rbuf), 0, (struct sockaddr *)&skt,
                         &sktlen)) < 0) {
     perror("recvfrom");
-    return;
+    return NULL;
   }
 	print_dhcpmsg((struct mydhcph *)rbuf, /*if_send=*/0);
 
-  // send
+	return (struct mydhcph *)rbuf;
+}
+
+void send_to_client(struct client *client, const struct mydhcph *msg, size_t msglen){
+	int s, count;
+	struct sockaddr_in skt;
+	in_port_t port;
+
+	// socket
+	if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+		fprintf(stderr, "cannot open socket");
+		return;
+	}
+
+	// set sockaddr_in
+	memset(&skt, 0, sizeof(skt));
+	skt.sin_family = AF_INET;
+	port = 51230;
+	skt.sin_port = htons(port);
+	skt.sin_addr = client->id;
+	
+	// send
+	if ((count = sendto(s, msg, msglen, 0, (struct sockaddr *)&skt, sizeof(skt))) < 0){
+		perror("sendto");
+		return;
+	}
+	print_dhcpmsg((struct mydhcph *)msg, /*if_send=*/1);
+}
+
+void wait_discover(){
+	struct mydhcph *msg;
+
+	msg = wait_client();
+	if (!msg){
+		return;
+	}
+
+	client.status = 0;
+	client.ttlcounter = DEFAULT_TTL;
+	memcpy(&(client.id), &msg->ipaddr, 4);
+	client.ttl = DEFAULT_TTL;
+
+	free(msg);
+}
+
+void send_offer(){
+	struct in_addr addr, netmask;
+
+	struct mydhcph msg = {0};
+	msg.type = OFFER;
+	msg.code = SUCCESS;
+	msg.ttl = DEFAULT_TTL;
+	// TODO: create avirable address list
+	inet_aton("192.168.210.100", &addr);
+	msg.ipaddr = *(uint32_t *)&addr;
+	inet_aton("255.255.255.0", &netmask);
+	msg.netmask = *(uint32_t *)&netmask;
+	send_to_client(&client, &msg, sizeof(msg));
 }
 
 int main() {
@@ -84,8 +148,13 @@ int main() {
   for (;;) {
     switch (status) {
     case INIT:
-      wait_client();
+			init();
       change_status(WAIT_DISCOVER);
+			break;
+		case WAIT_DISCOVER:
+      wait_discover();
+			send_offer();
+      change_status(WAIT_REQUEST);
       for (;;)
         ;
     }
