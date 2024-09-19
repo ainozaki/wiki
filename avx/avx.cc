38f6fd89714c1ae0603eee719c575976a706f8ce
ainozaki <aiai10piano@gmail.com>
2024-09-19 19:35:58 +0900

    fix avx

--- avx/avx.cc
+++ avx/avx.cc
@@ -37,8 +37,7 @@ template <int Shift> void _mm512_aesenc_epi128_emulate(__m512i &res, const __m51
     res = _mm512_inserti64x2(res, res_aes_128, Shift);
 }
 
-// test _mm512_aesenc_epi128
-void test_vaes(){
+void test_mm512_aesenc_epi128(){
     // VAES
     const __m512i v0 = _mm512_set_epi64(0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10);   
     const __m512i key = _mm512_set_epi64(0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20);
@@ -54,9 +53,9 @@ void test_vaes(){
     // Compare
     __mmask8 cmp = _mm512_cmpeq_epi64_mask(res_aes_512, res_aes_without_vaes);
     if (cmp == 0xFF) {
-        printf("AES: VAES result == Emulated result\n");
+        printf("Test __mm512_aesenc_epi128 ... OK\n");
     } else {
-        printf("AES: VAES result != Emulated result\n");
+        printf("Test __mm512_aesenc_epi128 ... FAIL\n");
     }
 }
 
@@ -84,7 +83,7 @@ __m512i _mm512_aesenclast_epi128_emulate(__m512i a, __m512i RoundKey){
     return res;
 }
 
-void test__mm512_aesenclast_epi128(){
+void test_mm512_aesenclast_epi128(){
     // VAES
     const __m512i v0 = _mm512_set_epi64(0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10);   
     const __m512i key = _mm512_set_epi64(0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20);
@@ -95,9 +94,9 @@ void test__mm512_aesenclast_epi128(){
     // Compare
     __mmask8 cmp = _mm512_cmpeq_epi64_mask(res, res_emulated);
     if (cmp == 0xFF) {
-        printf("Aesenclast: VAES result == Emulated result\n");
+        printf("Test __mm512_aesenclast_epi128 ... OK\n");
     } else {
-        printf("Aesenclast: VAES result != Emulated result\n");
+        printf("Test __mm512_aesenclast_epi128 ... FAIL\n");
     }
 }
 
@@ -106,6 +105,34 @@ int extract_bit(const char *ptr, int offset_bit, int bits){
     assert(offset_bit % 8 == 0);
     uint8_t *ptr_i = (uint8_t *)ptr;
     return ptr_i[offset_bit / 8] & ((1 << bits) - 1);
+}
+
+__m512i _mm512_permutexvar_epi8_emulate(const __m512i &idx, const __m512i &a){
+    alignas(alignof(__m512i)) unsigned char res[sizeof(__m512i)] = {0};
+    const char *a_ptr = (char *)&a;
+    const char *idx_ptr = (char *)&idx;
+    for (int j = 0; j < 64; j++) {
+        const int i = j * 8;
+        const int offset = extract_bit(idx_ptr, i, 6);
+        memcpy((char *)res + j, a_ptr + offset, 1);
+    }
+    return *(__m512i *)res;
+}
+
+void test_mm512_permutexvar_epi8(){
+    const __m512i a = _mm512_set_epi64(0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10);   
+    const __m512i idx = _mm512_set_epi64(0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF);
+    __m512i res = _mm512_permutexvar_epi8(idx, a);
+
+    __m512i res_emulated = _mm512_permutexvar_epi8_emulate(idx, a);
+
+    // Compare
+    __mmask8 cmp = _mm512_cmpeq_epi64_mask(res, res_emulated);
+    if (cmp == 0xFF) {
+        printf("Test __mm512_permutexvar_epi8 ... OK\n");
+    } else {
+        printf("Test __mm512_permutexvar_epi8 ... FAIL\n");
+    }  
 }
 
 __m512i _mm512_permutex2var_epi8_emulate(const __m512i &a, const __m512i &idx, const __m512i &b){
@@ -125,8 +152,7 @@ __m512i _mm512_permutex2var_epi8_emulate(const __m512i &a, const __m512i &idx, c
     return *(__m512i *)res;
 }
 
-// test _mm512_permutex2var_epi8
-void test_avx512_vbmi(){
+void test_mm512_permutex2var_epi8(){
     const __m512i a = _mm512_set_epi64(0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10);   
     const __m512i b = _mm512_set_epi64(0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20);   
     const __m512i idx = _mm512_set_epi64(0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF);
@@ -137,9 +163,9 @@ void test_avx512_vbmi(){
     // Compare
     __mmask8 cmp = _mm512_cmpeq_epi64_mask(res, res_emulated);
     if (cmp == 0xFF) {
-        printf("Permutex2var AVX512_VBMI result == Emulated result\n");
+        printf("Test __mm512_permutex2var_epi8 ... OK\n");
     } else {
-        printf("Permutex2var AVX512_VBMI result != Emulated result\n");
+        printf("Test __mm512_permutex2var_epi8 ... FAIL\n");
         std::cout << "Input: " << std::endl;
         showAVX512(a);
         showAVX512(b);
@@ -151,32 +177,31 @@ void test_avx512_vbmi(){
     }
 }
 
-void _mm512_mask_compressstoreu_epi16_emulate(void *res, const __m512i &v0, const __mmask32 &k){
-    char *base = (char *)res;
-    for (int i = 0; i < 32; i++){
-        const int j = i * 16;
-        int k_i = (k >> i) & 1;
-        if (k_i) {
-            memcpy((char *)base, (char *)&v0 + j, 2);
+void _mm512_mask_compressstoreu_epi16_emulate(void* base_addr, __mmask32 k, __m512i a){
+    char *base = (char *)base_addr;
+    for (int j = 0; j < 32; j++){
+        const int i = j * 16;
+        int k_j = (k >> j) & 1;
+        if (k_j) {
+            memcpy((char *)base, (char *)&a + i / 8, 2);
             base += 2;
         }
     }
 }
 
-// test _mm512_mask_compressstoreu_epi16
-void test_avx512_vbmi2(){
+void test_mm512_mask_compressstoreu_epi16(){
     alignas(alignof(__m512i)) unsigned char res[sizeof(__m512i)] = {0};
     alignas(alignof(__m512i)) unsigned char res_emulated[sizeof(__m512i)] = {0};
     
-    const __m512i a = _mm512_set_epi64(0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10);
-    const __mmask32 k = 16;
+    const __m512i a = _mm512_set_epi64(0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF);
+    const __mmask32 k = 15;
     _mm512_mask_compressstoreu_epi16(res, k, a);
 
-    _mm512_mask_compressstoreu_epi16_emulate(res_emulated, a, k);
+    _mm512_mask_compressstoreu_epi16_emulate(res_emulated, k, a);
     
     // Compare
     if (memcmp(res, (char *)&res_emulated, sizeof(__m512i)) == 0) {
-        printf("Compress: AVX512_VBMI2 result == Emulated result\n");
+        printf("Test _mm512_mask_compressstoreu_epi16 ... OK\n");
     } else {
         std::cout << "Input: " << std::endl;
         showAVX512(*(__m512i *)&a);
@@ -185,15 +210,16 @@ void test_avx512_vbmi2(){
         showAVX512(*(__m512i *)res);
         std::cout << "Emulation: " << std::endl;
         showAVX512(*(__m512i *)res_emulated);
-        printf("Compress: AVX512_VBMI2 result != Emulated result\n");
+        printf("Test _mm512_mask_compressstoreu_epi16 ... FAIL\n");
     }
 }
 
 int main(int argc, char **argv) {
     test_avx2();
     test_avx512();
-    test_vaes();
-    test__mm512_aesenclast_epi128();
-    test_avx512_vbmi();
-    test_avx512_vbmi2();
+    test_mm512_aesenc_epi128();
+    test_mm512_aesenclast_epi128();
+    test_mm512_permutexvar_epi8();
+    test_mm512_permutex2var_epi8();
+    test_mm512_mask_compressstoreu_epi16();
 }
